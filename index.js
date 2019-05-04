8da0047b74a77c3d227114ee8d7a841d69e38a62
tutanck <tutanck@gmail.com>
2019-05-04 02:04:16 +0200

    norm

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-(function(){
+(function() {
   'use strict';
 
-  const fs = require("fs");
-  const nodeFS = require("node-fs");
+  const fs = require('fs');
+  const nodeFS = require('node-fs');
   const argv = require('yargs').argv;
-  
+
   // folderName = FooBar      -- required
   // path = /Some/Sub/Folder  -- optional
   // noState = 'no-state'     -- optional
-  
+
   const folderName = argv._[0];
   let path;
   let noState;
@@ -16,36 +16,44 @@
   // Within create-react-apps yargs doesn't pick up on flags or options
   // so doing this hackery to make it work
 
-  path = argv._.find(val => /[\/]/.test(val))
-  noState = argv._.find(val => val === 'no-state')
-  
+  path = argv._.find(val => /[\/]/.test(val));
+  noState = argv._.find(val => val === 'no-state');
+
   if (!folderName) {
-    console.log('[ERROR]: Must supply a component name')
+    console.log('[ERROR]: Must supply a component name');
     return;
   }
-  
+
   let relativePath = './src';
   if (path) {
-    relativePath += path
+    relativePath += path;
   }
   relativePath += `/${folderName}`;
-  
-  fs.stat(relativePath, (err) => {
+
+  fs.stat(relativePath, err => {
     if (!err) {
       console.log(`[ERROR]: ${folderName} already exists at ${relativePath}`);
-    }
-    else if (err.code === 'ENOENT') {
+    } else if (err.code === 'ENOENT') {
       try {
         nodeFS.mkdirSync(relativePath, 0o777, true);
-        fs.writeFileSync(`${relativePath}/${folderName}.js`, setupJsFile(folderName));
-        fs.writeFileSync(`${relativePath}/${folderName}.css`, setupCSSFile(folderName));
-        fs.writeFileSync(`${relativePath}/${folderName}.test.js`, setupJsTestFile(folderName));
+        fs.writeFileSync(
+          `${relativePath}/${folderName}.js`,
+          setupJsFile(folderName)
+        );
+        fs.writeFileSync(
+          `${relativePath}/${folderName}.css`,
+          setupCSSFile(folderName)
+        );
+        fs.writeFileSync(
+          `${relativePath}/${folderName}.test.js`,
+          setupJsTestFile(folderName)
+        );
       } catch (err) {
-        console.error(err.message)
+        console.error(err.message);
       }
     }
   });
-  
+
   function setupJsFile(folderName) {
     if (noState) {
       return `import React from 'react';
@@ -69,12 +77,12 @@ export default class ${folderName} extends Component {
   }
 }
   `;
-  };
-  
+  }
+
   function setupCSSFile(folderName) {
     return `/* styling for ${folderName} Component */`;
   }
-  
+
   function setupJsTestFile(folderName) {
     return `import React from 'react';
 import ReactDOM from 'react-dom';
@@ -89,4 +97,4 @@ describe('${folderName} Component', () => {
 })
 `;
   }
-}())
+})();
